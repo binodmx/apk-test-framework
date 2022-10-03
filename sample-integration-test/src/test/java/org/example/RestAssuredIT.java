c3bc481823259cc058e516a09313aef33fa36db6
binodmx <binod@wso2.com>
2022-10-03 10:15:04 +0530

    Add rest-assured tests

---
+++ sample-integration-test/src/test/java/org/example/RestAssuredIT.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class RestAssuredIT {
+
+    @Test
+    public void getUsers() {
+        given().when().get("https://reqres.in/api/users?page=2")
+                .then().assertThat().body("page", equalTo(2));
+    }
+}
